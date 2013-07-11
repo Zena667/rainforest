f588570eaadff8bc854d142f9633ba56ab7675fd
Zena B <zena667@gmail.com>
2013-07-11 15:27:36 -0400

    setup suite to use factory girl

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,5 +5,12 @@ class Product < ActiveRecord::Base
 
   validates :description, :name, :presence => true
   validates :price_in_cents, :numericality => {:only_integer => true}
+
+# def dollar_convert
+#   number_to_currency(@product[:price_in_cents].to_f/100)
+# end  
+
 end
+
+
 
---
+++ test/factories/categories.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :product do
+    name "Lamp"
+    description "A desk lamp perfect for the modern office."
+    price_in_cents 10000
+  end
+end
