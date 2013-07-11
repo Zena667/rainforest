01df6586ca0bcc090ffd34d6981f659dd7166bee
Zena B <zena667@gmail.com>
2013-07-11 14:48:42 -0400

    Adding user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
   has_secure_password
   validates_presence_of :password, :on => :create
+  validates_presence_of :name
   has_many :reviews
   has_many :products, :through => :reviews
 end
